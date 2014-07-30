2f89f5c52210fa20aba90e131e8e2a27e47d5103
Chris0lsen <chris@chrisolsen.net>
2014-07-30 09:45:29 -0500

    Added files
    Added script and example project file

---
+++ mbc_script.jsx
@@ -0,0 +1,89 @@
+﻿/*************************************************************
+ **** Author: Chris Olsen
+ **** Date: 7/30/2014
+ **** Description: "Movie Body Counts" script for internal 
+ ****                         AdKarma use. Streamlines creation of
+ ****                         videos for Movie Body Counts channel.
+ **** Last revised: 7/30/2014
+ **** 
+ ****
+ ************************************************************/
+
+/****************************
+    draw GUI
+****************************/
+
+
+
+/*****************************
+    locate necessary files
+*****************************/
+    //locate bumper on fs
+
+    //locate subscribe button on fs
+
+    //locate clips for end bumper on fs
+
+    //locate grid for end bumper on fs
+
+
+
+/*****************************
+    create all solids
+******************************/
+
+    //fade out
+
+    //number matte
+    
+    //rating matte
+    
+    //end bumper matte
+    
+    //splatter1
+    
+    //splatter2
+    
+ /*******************************
+     create all text layers
+ ********************************/
+
+    //BODY COUNT:
+
+    //Counter
+
+    //Rating
+
+    //Total count
+
+    //Subscribe
+
+    //numberHolder
+
+/*****************************
+    create all precomps
+ ****************************/
+
+    //text
+
+    //number
+
+    //count
+
+    //sub_button
+
+/****************************************************
+    Add effects and expressions to 'text' precomp
+*****************************************************/
+
+/****************************************************
+    Add effects and expressions to 'number' precomp
+*****************************************************/
+
+/****************************************************
+    Add effects and expressions to 'count' precomp
+*****************************************************/
+
+/****************************************************
+    Add effects and expressions to 'sub_button' precomp
+*****************************************************/
